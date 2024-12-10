51cde0d4d128ddf24dcb3079054f74714c412466
hejzbh <hazimba123@gmail.com>
2024-12-10 22:41:36 +0100

    small fix

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -24,7 +24,7 @@ const Button = ({
   return (
     <button
       title={dataTitle}
-      className={`py-3 px-7 rounded-md transition-all duration-300 ease-in-out hover:md:opacity-90 relative overflow-hidden  min-w-40  shadow-2xl  before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500  hover:before:w-2/4  hover:after:w-2/4 after:z-[-1] before:z-[-1]  ${variantClasses[variant]} ${className}`}
+      className={`py-3 px-7 rounded-md transition-all duration-200 ease-in-out hover:md:opacity-90 relative overflow-hidden  min-w-40  shadow-2xl  before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:duration-500 after:absolute after:right-0 after:top-0 after:h-full after:w-0 after:duration-500  hover:before:w-2/4  hover:after:w-2/4 after:z-[-1] before:z-[-1]  ${variantClasses[variant]} ${className}`}
     >
       {children}
     </button>
