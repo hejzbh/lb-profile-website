13eec7ec1897559d470d0538ad4d0e0ab8a78be5
hejzbh <hazimba123@gmail.com>
2024-12-11 18:12:50 +0100

    edit

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -14,10 +14,11 @@ const Hero = ({ className = "" }: HeroProps) => {
       <Image
         src={bgImg}
         fill
-        className="z-[-1] object-cover"
+        className="z-[-2] object-cover"
         alt="Background"
         draggable={false}
       />
+      <div className="absolute top-0 left-0 w-full h-full bg-white z-[-1] opacity-10"></div>
       <div className="container flex flex-col justify-center items-start min-h-[80dvh]">
         <Title size="xl" className="max-w-[730px] mb-4" variant="h1">
           PVC-PROFILSYSTEME FUR FENSTER UND TUREN, EINZELHANDEL
