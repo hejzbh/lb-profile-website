456f8deb5ee7b414eeae9662dd4e2bb7e0a66b22
hejzbh <hazimba123@gmail.com>
2024-12-12 19:43:11 +0100

    edit

--- components/ui/Text.tsx
+++ components/ui/Text.tsx
@@ -12,8 +12,8 @@ type TextProps = {
 const sizeClasses = {
   xs: "text-[0.625rem] sm:text-[0.75rem] md:text-[0.875rem]",
   sm: "text-[0.95rem] sm:text-[0.875rem] md:text-[0.9rem]",
-  md: "text-[1.020rem] sm:text-[0.9375rem] md:text-[0.950rem] xl:text-[1.050rem]",
-  lg: "text-[1.1rem] sm:text-[1.1rem] md:text-[1.125rem]",
+  md: "text-[1.050rem] sm:text-[0.9375rem] md:text-[0.950rem] xl:text-[1.050rem]",
+  lg: "text-[1.125rem] sm:text-[1.1rem] md:text-[1.125rem]",
   xl: "text-[1.4rem] sm:text-[1.25rem] md:text-[1.75rem] lg:text-[1.3rem]",
   biggerSm: "text-[1.2rem] sm:text-[0.875rem] md:text-[0.9375rem]",
 };
