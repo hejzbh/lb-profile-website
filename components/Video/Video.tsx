e6bb3a65622f1e535db4a452a6c13cae641a6cca
hejzbh <hazimba123@gmail.com>
2024-12-15 23:13:39 +0100

    edit

--- components/Video/Video.tsx
+++ components/Video/Video.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Title from "@/components/ui/Title";
 import bgImg from "@/public/images/video-bg.webp";
