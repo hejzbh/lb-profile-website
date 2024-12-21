76fe9e49d9b9b42edaed92c5ae6d40fc0f2680f5
hejzbh <hazimba123@gmail.com>
2024-12-21 21:49:51 +0100

    edit

--- components/ui/BackgroundImage.tsx
+++ components/ui/BackgroundImage.tsx
@@ -5,12 +5,14 @@ type BackgroundImageProps = {
   className?: string;
   src: StaticImageData;
   children: React.ReactNode;
+  childrenClassName?: string;
 };
 
 const BackgroundImage = ({
   className = "",
   src,
   children,
+  childrenClassName = "",
 }: BackgroundImageProps) => {
   return (
     <div className={`relative z-[1]  ${className}`}>
@@ -23,7 +25,9 @@ const BackgroundImage = ({
         alt="LB-Profile"
       />
       <div className="absolute top-0 left-0 w-full h-full bg-black/50 z-[-1]"></div>
-      <div className="container mx-auto min-h-[37dvh] md:min-h-[50dvh] pt-28 py-10 z-[2] flex flex-col justify-center items-center">
+      <div
+        className={`container mx-auto min-h-[37dvh] md:min-h-[50dvh] pt-28 py-10 z-[2] flex flex-col justify-center items-center ${childrenClassName}`}
+      >
         {children}
       </div>
     </div>
