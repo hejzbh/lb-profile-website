52796a5b45da288a4d416bbfee6188aff9c1e4bd
hejzbh <hazimba123@gmail.com>
2025-03-09 19:32:25 +0100

    edit

--- components/Services/ServiceCard.tsx
+++ components/Services/ServiceCard.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import Title from "@/components/ui/Title";
 import Link from "next/link";
-import Text from "@/components/ui/Text";
 
 type ServiceCardProps = {
   className?: string;
@@ -17,7 +16,7 @@ const ServiceCard = ({ className = "", item }: ServiceCardProps) => {
       href={"/"}
       className={`transition block active:opacity-70 relative hover:md:opacity-90 hover:md:translate-y-[-3%] group ${className}`}
     >
-      <article>
+      <article className="flex flex-col justify-center items-center">
         {/** Image & Date */}
         <div className="relative">
           <Image
@@ -26,12 +25,8 @@ const ServiceCard = ({ className = "", item }: ServiceCardProps) => {
             width={550}
             height={550}
             loading="lazy"
-            className="w-full rounded-xl"
+            className="w-[300px] h-[280px] rounded-full"
           />
-
-          <Text className="bg-white text-black font-[600] group-hover:bg-textColors-hover group-hover:text-white max-w-fit px-4 py-2 absolute top-5 left-5">
-            {new Date(item.createdAt).toDateString()}
-          </Text>
         </div>
 
         {/** Title */}
